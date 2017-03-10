09e78033b5fd7b2e0d0f8fcbcabdf3c5449df931
Artur Veselovski <Artur Veselovski>
2017-03-10 10:27:32 +0200

    Esimerkki

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hei from './components/Hei.js';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           LAITETAAN HAISEMAAN <code>src/App.js</code> and save to reload.
         </p>
+		<Hei />
       </div>
     );
   }
---
+++ src/components/Hei.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hei extends Component {
+  render() {
+    return (
+		<p>Hei!</p>
+    );
+  }
+}
+
+export default Hei;
