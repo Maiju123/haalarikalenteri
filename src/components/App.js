90d0ef8354b8506b8dbd75a14cabac677dd18f83
k2322 <k2322@student.jamk.fi>
2017-03-21 07:08:55 -0400

    EditEvent Stepper added. Temporarily to App.js, will be moved to EditEvent.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import EventList from './EventList';
 import AppBarNav from './AppBarNav';
+import EditEventStepper from './EditEventStepper';
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     <div>
       <AppBarNav />
       <EventList />
+      <EditEventStepper />
     </div>
     );
   }
---
+++ src/components/EditEventStepper.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { Step, Stepper, StepLabel, } from 'material-ui/Stepper';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import AppBarNav from './AppBarNav';
+
+/**
+ * Horizontal steppers are ideal when the contents of one step depend on an earlier step.
+ * Avoid using long step names in horizontal steppers.
+ *
+ * Linear steppers require users to complete one step in order to move on to the next.
+ */
+class HorizontalLinearStepper extends React.Component {
+
+  state = { finished: false, stepIndex: 0, };
+
+  handleNext = () => {
+    const {stepIndex} = this.state;
+    this.setState({
+      stepIndex: stepIndex + 1,
+      finished: stepIndex >= 2,
+    });
+  };
+
+  handlePrev = () => {
+    const {stepIndex} = this.state;
+    if (stepIndex > 0) {
+      this.setState({stepIndex: stepIndex - 1});
+    }
+  };
+
+  getStepContent(stepIndex) {
+    switch (stepIndex) {
+      case 0:
+        return 'Tämä stepperi tulee Muokkaa tapahtumaa sivulle';
+      case 1:
+        return 'Älä huoli, tämä lähtee pois';
+      case 2:
+        return 'Tarvitaan vaan React Router ekaks';
+      default:
+        return 'Hahaha';
+    }
+  }
+
+  render() {
+    const {finished, stepIndex} = this.state;
+    const contentStyle = {margin: '0 16px'};
+
+    return (
+      <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
+        <Stepper activeStep={stepIndex}>
+          <Step>
+            <StepLabel> Kohta yks - hae muokattava tapahtuma</StepLabel>
+          </Step>
+          <Step>
+            <StepLabel> Kohta kaks - syötä salasana eventtiin</StepLabel>
+          </Step>
+          <Step>
+            <StepLabel> Kohta kolme - muokkaa tapahtumaa lomakkeessa </StepLabel>
+          </Step>
+        </Stepper>
+        <div style={contentStyle}>
+          {finished ? (
+            <p>
+              <a
+                href="#"
+                onClick={(event) => {
+                  event.preventDefault();
+                  this.setState({stepIndex: 0, finished: false});
+                }}
+              >
+                Click here
+              </a> to reset the example.
+            </p>
+          ) : (
+            <div>
+              <p>{this.getStepContent(stepIndex)}</p>
+              <div style={{marginTop: 12}}>
+                <FlatButton
+                  label="Palaa"
+                  disabled={stepIndex === 0}
+                  onTouchTap={this.handlePrev}
+                  style={{marginRight: 12}}
+                />
+                <RaisedButton
+                  label={stepIndex === 2 ? 'Valmis' : 'Seuraava'}
+                  primary={true}
+                  onTouchTap={this.handleNext}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HorizontalLinearStepper;
