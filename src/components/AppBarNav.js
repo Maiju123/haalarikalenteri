a385a3a8afdeb390f592f24b9a3a18a64a65564c
Aaro Lappalainen <aarolappalainen@gmail.com>
2017-04-18 20:46:58 +0300

    Navigation Menu changed to Drawer

--- src/components/AppBarNav.js
+++ src/components/AppBarNav.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import { AppBar, IconMenu, IconButton, MenuItem, } from 'material-ui';
-import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
-
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 import {
   Link
 } from 'react-router-dom';
 
-const AppBarNav = () => (
-  <AppBar
-    title="Haalarikalenteri"
-    iconElementLeft={
-      <IconMenu iconButtonElement={
-        <IconButton>
-          <NavigationMenu />
-        </IconButton>
-      }>
-        <MenuItem containerElement={<Link to="/" />} primaryText="Etusivu" />
-        <MenuItem containerElement={<Link to="/Lisaa" />} primaryText="Lisää tapahtuma" />
-        <MenuItem containerElement={<Link to="/Muokkaa" />} primaryText="Muokkaa tapahtumaa" />
-        <MenuItem containerElement={<Link to="/Info" />} primaryText="Info" />
-        <MenuItem containerElement={<Link to="/Terms" />}primaryText="Terms" />
-      </IconMenu>
-    }
-  />
-);
+import { AppBar, IconMenu } from 'material-ui';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 
-export default AppBarNav;
+export default class AppBarNav extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
+
+  render() {
+    return (
+      <AppBar onTouchTap={this.handleToggle} title="Haalarikalenteri" >
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem onTouchTap={this.handleClose} containerElement={<Link to="/" />} primaryText="Etusivu" />
+          <MenuItem onTouchTap={this.handleClose} containerElement={<Link to="/Lisaa" />} primaryText="Lisää tapahtuma" />
+          <MenuItem onTouchTap={this.handleClose} containerElement={<Link to="/Muokkaa" />} primaryText="Muokkaa tapahtumaa" />
+          <MenuItem onTouchTap={this.handleClose} containerElement={<Link to="/info" />} primaryText="Info" />
+          <MenuItem onTouchTap={this.handleClose} containerElement={<Link to="/terms" />} primaryText="Terms" />
+        </Drawer>
+      </AppBar>
+    );
+  }
+}
