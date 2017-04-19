0bf24926837a725252e660dcb605523f6b442942
K2791 <miro.ahola@gmail.com>
2017-04-19 20:57:53 +0300

    Responsive AddEventForm

--- src/components/AddEventPage.js
+++ src/components/AddEventPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 import AddEventForm from './AddEventForm';
+import './form.css';
 
 const style = {
   display: 'inline-block',
@@ -10,7 +11,7 @@ const style = {
 };
 
 const AddEventPage = () => (
-  <div>
+  <div className="add-event-page">
     <Paper style={style} zDepth={1}> 
     <AddEventForm />
     </Paper>
