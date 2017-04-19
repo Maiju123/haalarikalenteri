09528a82b35df9f2b401b0745b6be787d52dbd1a
Aaro Lappalainen <aarolappalainen@gmail.com>
2017-04-19 19:08:52 +0300

    Small hover effect for event's Card.

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,22 +4,34 @@ import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import Chip from 'material-ui/Chip';
 
 class Event extends Component {
-    
+
+  constructor(props) {
+    super(props);
+    this.state = {shadow: 1}
+}
+
+onMouseOver = () => this.setState({shadow: 2});
+onMouseOut = () => this.setState({shadow: 1});
+
     render() {
-        
+
         return (
 
-            <Card style={{marginTop: '5px', marginBottom: '5px'}}>
+            <Card style={{marginTop: '5px', marginBottom: '10px'}}
+              onMouseOver={this.onMouseOver}
+              onMouseOut={this.onMouseOut}
+              zDepth={this.state.shadow}
+              >
                 <CardHeader
-                    title={this.props.title} 
+                    title={this.props.title}
                     subtitle={this.props.date}
                     avatar={this.props.img}
-                    actAsExpander={true} 
-                    showExpandableButton={true} 
+                    actAsExpander={true}
+                    showExpandableButton={true}
                 />
-                <CardText 
-                    children={this.props.desc} 
-                    expandable={true} 
+                <CardText
+                    children={this.props.desc}
+                    expandable={true}
                 />
                 <CardActions style={{width: '100%', textAlign: 'right', width:'100%', boxSizing: 'border-box'}} >
                     {this.props.categories.map(function(category) {
